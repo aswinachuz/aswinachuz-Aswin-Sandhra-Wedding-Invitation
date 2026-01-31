df4c04f5141e822a8d4073e1d42b03b17fc7d560
Aswin U <33390087+aswinachuz@users.noreply.github.com>
2026-01-31 07:48:03 +0530

    Modify imageLabels to empty strings
    Updated imageLabels to be empty strings.

--- src/Components/gallery.jsx
+++ src/Components/gallery.jsx
@@ -6,7 +6,7 @@ const images = [
     "/3.JPG"
 ];
 
-const imageLabels = ["The Proposal", "Our First Trip", "Beach Days"];
+const imageLabels = ["", "", ""];
 
 export default function Gallery() {
   const [active, setActive] = useState(null);
